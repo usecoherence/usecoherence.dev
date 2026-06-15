37073590cce50beb850bbe677c695f6a7221d510
Nikolay Konovalov <konovalov.nk@gmail.com>
2026-06-15 22:24:46 +0200

    feat: add Prism syntax highlighting via eleventy-plugin-syntaxhighlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
